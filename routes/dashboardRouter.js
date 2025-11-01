1c24163f28fce5652c73b0fffd8623a55f201ab0
Richard Miguel <richardmiquel19@gmail.com>
2025-11-01 07:05:20 +0700

    add_dashboard_user

--- routes/dashboardRouter.js
+++ routes/dashboardRouter.js
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import prisma from "../config/prismaClient.js";
+import bcrypt from "bcryptjs";
 import {
   getAllSiswa,
   createSiswa,
@@ -56,5 +58,50 @@ router.get("/jadwal/:id/absensi", getAbsensiByJadwal);
 router.get("/rekapitulasi", getRekapitulasi);
 router.get("/rekapitulasi/download", downloadRekapitulasi);
 
+router.get("/user", async (req, res) => {
+  const users = await prisma.user.findMany({
+    orderBy: { id: "asc" },
+  });
+
+  res.render("dashboard-user", {
+    title: "User",
+    users});
+});
+
+router.get("/user/add", (req, res) => {
+  res.render("form-user", {
+    title: "Form User"
+  });
+});
+
+router.post("/user/add", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const hashed = await bcrypt.hash(password, 10);
+
+    await prisma.user.create({
+      data: { username, password: hashed },
+    });
+
+    res.redirect("/dashboard/user");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Gagal menambahkan user");
+  }
+});
+
+// POST /user/delete/:id -> hapus user
+router.post("/delete/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await prisma.user.delete({ where: { id: Number(id) } });
+    res.redirect("/dashboard/user");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Gagal menghapus user");
+  }
+});
 
 export default router;
